bb456d2e206a35ff4292e1e8a66dfc2976341d1e
stnolting <stnolting@gmail.com>
2022-08-27 11:07:29 +0200

    setup GitHub actions

---
+++ .github/check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo ""
+
+RELEASE=initial_test
+ARCHIVE=iverilog-27082022
+echo "Checking release $RELEASE, archive $ARCHIVE..."
+wget https://github.com/stnolting/icarus-verilog-prebuilt/releases/download/$RELEASE/$ARCHIVE.tar.gz
+ls -al
+mkdir $GITHUB_WORKSPACE/$ARCHIVE.tmp
+tar -xzf $ARCHIVE.tar.gz -C $GITHUB_WORKSPACE/$ARCHIVE.tmp/
+$GITHUB_WORKSPACE/$ARCHIVE.tmp/bin/iverilog -V
